13d85e141baf2e0f06aa94862a71aced91ea5888
Becky Smethurst <becky@lpprs.physics.nottingham.ac.uk>
2018-08-02 12:11:53 +0100

    package release structure

---
+++ snitch/functions.py
@@ -0,0 +1,302 @@
+import os
+import numpy as np
+
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
+
+from itertools import product
+
+import fsps
+
+from mangadap.proc.templatelibrary import TemplateLibrary
+from mangadap.proc.ppxffit import PPXFFit
+from mangadap.proc.stellarcontinuummodel import StellarContinuumModelBitMask
+
+from mangadap.proc.emissionlinemodel import EmissionLineModelBitMask
+from mangadap.proc.elric import Elric
+from mangadap.par.emissionlinedb import EmissionLineDB
+from mangadap.par.spectralfeaturedb import SpectralFeatureDBDef
+
+from mangadap.proc.spectralindices import SpectralIndices
+from mangadap.par.absorptionindexdb import AbsorptionIndexDB
+from mangadap.par.bandheadindexdb import BandheadIndexDB
+
+from astropy import constants as con
+from astropy import units as un
+c = con.c.to(un.km/un.s).value
+
+
+######################################################################################################
+#
+# August 1st 2018, RJS
+#
+# This file contains all the functions needed to generate synthetic spectra for a defined SFH using 
+# FSPS, measure these spectra using the MaNGA DAP and save it for later use as a look up table. 
+# A user may wish to adapt these functions, either to define their own star formation histories, or 
+# if they wish to use another method of spectral fitting to give spectral parameters. 
+#
+# Documentation is provided but if you have any further questions please email rjsmethurst@gmail.com
+#
+######################################################################################################
+
+
+def expsfh(tq, tau, time):
+    """ This function when given an array of [tq, tau] values will calcualte the SFR at all times. 
+    First calculate the sSFR at all times as defined by Peng et al. (2010) - then the SFR at the specified time of 
+    quenching, tq and set the SFR at this value  at all times before tq. Beyond this time the SFR is an exponentially 
+    declining function with timescale tau. 
+        
+        INPUT:
+        :tau:
+        The exponential timescale decay rate of the star formation history in Gyr. Allowed range from the rest of the functions is 0 < tau [Gyr] < 5. Shape (1, M, N). 
+        
+        :tq: 
+        The time at which the onset of quenching begins in Gyr. Allowed ranges from the beginning to the end of known cosmic time. Shape (1, M, N). 
+        
+        e.g:
+        
+        tqs = np.linspace(0, 14, 10)
+        taus = np.linspace(0, 4, 5)
+        
+        tq = tqs.reshape(1,-1,1).repeat(len(taus), axis=2)
+        tau = taus.reshape(1,1,-1).repeat(len(tqs), axis=1)
+        
+        :time:
+        An array of time values at which the SFR is calcualted at each step. Shape (T, 1, 1)
+        
+        RETURNS:
+        :sfr:
+        Array of the same dimensions of time containing the sfr at each timestep. Shape (T, M, N). 
+
+    A user can define their own SFH function here. Just make sure that the array returned is of the shape (T, M, N) as outlined above.
+
+        """
+
+    ssfr = 2.5*(((10**10.27)/1E10)**(-0.1))*(time/3.5)**(-2.2)
+    c = np.apply_along_axis(lambda a: a.searchsorted(3.0), axis = 0, arr = time)    
+    ssfr[:c.flatten()[0]] = np.interp(3.0, time.flatten(), ssfr.flatten())
+    c_sfr = np.interp(tq, time.flatten(), ssfr.flatten())*(1E10)/(1E9)
+    ### definition is for 10^10 M_solar galaxies and per gyr - convert to M_solar/year ###
+    sfr = np.ones_like(time)*c_sfr
+    mask = time <= tq
+    sfrs = np.ma.masked_array(sfr, mask=mask)
+    times = np.ma.masked_array(time-tq, mask=mask)
+    sfh = sfrs*np.exp(-times/tau)
+    return sfh.data
+
+
+def generate_spectra(sfr):
+
+    """ 
+    This function allows the user to generate many spectra for a defined SFR at given ages, at user defined time steps and metallicities. 
+
+    INPUT:
+        :sfr:
+        The SFRs defined at each age for any number of sfhs, H. Shape (H, SFR)
+       
+    GLOBAL PARAMETERS: 
+        :ages: 
+        The times at which the sfr is defined for any number of sfhs, H. Shape (H, SFR). 
+        
+        :time_steps:
+        An array of time values for which a spectrum with the given sfr is returned. Shape (T,)
+
+        :zmets:
+        An array of model metallicity values (range 1-22, see FSPS details) for which a spectrum with the given sfr is returned. Shape (Z,)
+        
+    RETURNS:
+        :fsps_flux:
+        Fluxes at each manga wavelength, M for the number of sfhs input at each time_step and zmet combination. Shape (H*T*Z, M). 
+
+    """
+
+    sp = fsps.StellarPopulation(compute_vega_mags=False, zcontinuous=0, zmet=18, sfh=3, dust_type=2, dust2=0.2, 
+        add_neb_emission=True, add_neb_continuum=False, imf_type=1, add_dust_emission=True, min_wave_smooth=3600, max_wave_smooth=7500, smooth_velocity=True, sigma_smooth=77.)
+
+    sp.set_tabular_sfh(age = ages, sfr=sfr)
+
+    def time_spec(params):
+        sps = sp.get_spectrum(tage=params[0], zmet=params[1])[1]
+        return sps
+
+    fsps_wave = sp.get_spectrum()[0]
+    fsps_spec = np.array(list(map(time_spec, list(product(time_steps, zmets)))))
+
+    manga_wave = np.load("manga_wavelengths_AA.npy")
+
+    f = interpolate.interp1d(fsps_wave, fsps_spec)
+    fluxes = f(manga_wave)
+
+    return fsps_flux 
+
+
+def measure_spec(fluxes):
+    """ This function takes an array of spectra of shape (# of spectra, # of wavelengths) and returns the pre-defined emission line and 
+    absorption features in each spectra in two recorded arrays of shape (# of spectra,). The recorded arrays can be accessed to give the
+    quantities returned by the MaNGA DAP, e.g. ["EW"], ["EWERR"], ["INDX"]. 
+
+    The user can change the emission and absorption quantities returned by specifying different emission line and absorption feature 
+    databases.
+
+    INPUTS
+    :fluxes:
+    Fluxes at each manga wavelength, M for any number of spectra, X. Shape (X, M).
+
+    OUTPUTS
+    :em_model_eml_par: 
+    Recorded array containing the emisison line parameter measurements from the defined user database. Shape (X, ).
+
+    :indx_measurement:
+    Recorded array containing the absorption feature parameter measurements from the defined user database. Shape (X, ).
+
+    """
+    tpl = TemplateLibrary("MILESHC",
+                            match_to_drp_resolution=False,
+                            velscale_ratio=1,    
+                            spectral_step=1e-4,
+                            log=True,
+                            directory_path=".",
+                            processed_file="mileshc.fits",
+                            clobber=True)
+
+    # Instantiate the object that does the fitting
+    contbm = StellarContinuumModelBitMask()
+    ppxf = PPXFFit(contbm)
+
+    # Define the absorption and bandhead feature databases
+    define_abs_db = SpectralFeatureDBDef(key='USERABS',
+                              file_path='extindxsnitch.par')
+    abs_db = AbsorptionIndexDB(u"USERABS", indxdb_list=define_abs_db)
+
+    band_db = BandheadIndexDB(u"BHBASIC")
+    global indx_names
+    indx_names = np.hstack([abs_db.data["name"], band_db.data["name"]])
+
+    # Define the emission line feature database
+    specm = EmissionLineModelBitMask()
+    elric = Elric(specm)
+    global emlines
+    define_em_db = SpectralFeatureDBDef(key='USEREM',
+                              file_path='elpsnitch.par')
+    emlines  = EmissionLineDB(u"USEREM", emldb_list=define_em_db)
+    
+    # Check to see if a single spectra has been input. If the single spectra is shape (# of wavelengths,) then reshape 
+    # to give (1, #number of wavelengths)
+    if fluxes.shape[0] == len(manga_wave):
+        fluxes = fluxes.reshape(1,-1)
+    else:
+        pass
+    nspec = fluxes.shape[0]
+
+
+    # Provide the guess redshift and guess velocity dispersion
+    guess_redshift = np.full(nspec, 0.0001, dtype=float)
+    guess_dispersion = np.full(nspec, 77.0, dtype=float)
+
+    # Perform the fits to the continuum, emission lines and absorption features
+    model_wave, qmodel_flux, model_mask, model_par  = ppxf.fit(tpl["WAVE"].data.copy(), tpl["FLUX"].data.copy(), fsps_wave, fluxes, np.ones_like(fluxes), guess_redshift, guess_dispersion, iteration_mode="none", velscale_ratio=1, degree=8, mdegree=-1, moments=2, quiet=True)
+    em_model_wave, qem_model_flux, qem_model_base, em_model_mask, em_model_fit_par, em_model_eml_par = elric.fit(fsps_wave, fluxes, emission_lines=emlines, ivar=np.ones_like(fluxes), sres=np.ones_like(fluxes), continuum=model_flux, guess_redshift = model_par["KIN"][:,0]/c, guess_dispersion=model_par["KIN"][:,1], base_order=1, quiet=True)
+    indx_measurements = SpectralIndices.measure_indices(absdb=abs_db, bhddb=band_db, wave=fsps_wave, flux=fluxes, ivar=np.ones_like(fluxes), mask=None, redshift=model_par["KIN"][:,0]/c, bitmask=None)
+
+    # Close all plots generated by the MaNGA DAP pipeline
+    plt.close("all")
+    plt.cla()
+    plt.clf()
+
+    return em_model_eml_par, indx_measurement
+
+
+def save_lookup(spectra):
+
+    """
+    This function takes the output from the measure_spec function and turns the measurements, errors and masked values into a 
+    useable format that are then saved as seperate compressed npz arrays.
+
+    INPUTS:
+        :spectra:
+        This is an array of spectra output from the generate_spectra function. We do not recommend inputting large arrays of spectra at once.
+        Instead, we recommend chunking this array with N = 10-100 spectra in each chunk. The fluxes should be reported at each manga wavelength, M 
+        so that the shape is (N, M).
+
+    OUTPUTS:
+        :npz files:
+        The compressed npz files that are saved will all have the shape (N, 7) - as there are 7 spectral parameters recorded for SNITCH. 
+        If the files already exist, the measurements will be appended on to the end of the existing file. If starting a new run, remember to 
+        either delete or rename any files from previous runs so that you know which values are relevant. 
+    """
+
+    eml, idm = measure_spec(spectra)
+    emls = eml["EW"][:, np.where(emlines['name']=='Ha')].reshape(-1,1)
+    idms =idm["INDX"][:,np.logical_or(np.logical_or(indx_names=='D4000', indx_names=='Fe5270'), np.logical_or(np.logical_or(indx_names=='Fe5335', indx_names=='HDeltaA'),  np.logical_or(indx_names=='Hb', indx_names=='Mgb')))].reshape(-1,6)
+
+    emls_err = eml["EWERR"][:, np.where(emlines['name']=='Ha')].reshape(1,-1)
+    idms_err =idm["INDXERR"][:,np.logical_or(np.logical_or(indx_names=='D4000', indx_names=='Fe5270'), np.logical_or(np.logical_or(indx_names=='Fe5335', indx_names=='HDeltaA'),  np.logical_or(indx_names=='Hb', indx_names=='Mgb')))].reshape(1,-1)
+    
+    emls_mask = eml["MASK"][:, np.where(emlines['name']=='Ha')].reshape(-1,1)
+    idms_mask =idm["MASK"][:,np.logical_or(np.logical_or(indx_names=='D4000', indx_names=='Fe5270'), np.logical_or(np.logical_or(indx_names=='Fe5335', indx_names=='HDeltaA'),  np.logical_or(indx_names=='Hb', indx_names=='Mgb')))].reshape(-1,6)
+
+    lu = np.append(emls, idms, axis=1).reshape(-1, 7) # Halpha 0th, Hbeta 2nd, MgB 3rd, Fe5270 4th, Fe5335 5th, HDeltaA 6th, D4000 7th
+    lu_err = np.append(emls_err, idms_err, axis=1).reshape(-1,7) # Halpha 0th, Hbeta 2nd, MgB 3rd, Fe5270 4th, Fe5335 5th, HDeltaA 6th, D4000 7th
+    lu_mask = np.append(emls_mask, idms_mask, axis=1).reshape(-1, 7) # Halpha 0th, Hbeta 2nd, MgB 3rd, Fe5270 4th, Fe5335 5th, HDeltaA 6th, D4000 7th
+
+    with l:
+        if os.path.isfile("spectral_parameter_measurements.npz"):
+            with np.load("spectral_parameter_measurements.npz") as sp_lu:
+                np.savez("spectral_parameter_measurements.npz", lookup=np.append(sp_lu["lookup"], lu, axis=0))
+            with np.load("spectral_parameter_measurements_mask.npz") as sp_lu_mask:
+                np.savez("spectral_parameter_measurements_mask.npz", lookupmask=np.append(sp_lu_mask["lookupmask"], lu_mask, axis=0))
+            with np.load("spectral_parameter_measurements_error.npz") as sp_lu_err:
+                np.savez("spectral_parameter_measurements_error.npz", lookupmask=np.append(sp_lu_err["lookuperr"], lu_err, axis=0))
+            
+        else:
+            np.savez("spectral_parameter_measurements.npz", lookup=lu)
+            np.savez("spectral_parameter_measurements_mask.npz", lookupmask=lu_mask)
+            np.savez("spectral_parameter_measurements_error.npz", lookuperr=lu_err)          
+
+
+def walker_plot(samples, nwalkers, ndim=3, limit=-1, truth=[np.nan, np.nan, np.nan]):
+
+    """ Plotting function to visualise the steps of the walkers in each parameter dimension. 
+        
+        :samples:
+        Array of shape (nsteps*nwalkers, 3) produced by the sample function in snitch_mcmc.py
+        
+        :nwalkers:
+        The number of walkers that step around the parameter space used to produce the samples by the sample function. M
+        ust be an even integer number larger than ndim.
+
+        :ndim:
+        Number of parameters fit by emcee. Default is 3. Optional. 
+        
+        :limit:
+        Integer value less than nsteps to plot the walker steps to. Optional. 
+
+        :truth:
+        Actual values of [Z, tq, tau] if known - can also be used to input the 50th percentile to show the quoted values. Optional. 
+
+        RETURNS:
+        :fig:
+        The figure object
+        """
+    s = samples.reshape(nwalkers, -1, ndim)
+    s = s[:,:limit, :]
+    fig = plt.figure(figsize=(8,12))
+    ax1 = plt.subplot(3,1,1)
+    ax2 = plt.subplot(3,1,2)
+    ax3 = plt.subplot(3,1,3)
+    ax1.plot(s[:,:,0].T, 'k')
+    ax1.axhline(truth[0], color='r')
+    ax2.plot(s[:,:,1].T, 'k')
+    ax2.axhline(truth[1], color='r')
+    ax3.plot(s[:,:,2].T, 'k')
+    ax3.axhline(truth[2], color='r')
+    ax1.tick_params(axis='x', labelbottom='off')
+    ax2.tick_params(axis='x', labelbottom='off')
+    ax3.set_xlabel(r'step number')
+    ax1.set_ylabel(r'$Z$')
+    ax2.set_ylabel(r'$t_{quench}$')
+    ax3.set_ylabel(r'$\tau$')
+    plt.subplots_adjust(hspace=0.1)
+
+    return fig
---
+++ snitch/snitch_mcmc.py
@@ -0,0 +1,228 @@
+import numpy as np
+import emcee
+import time
+import os
+from astropy.cosmology import Planck15
+
+from itertools import product
+
+from scipy.interpolate import NearestNDInterpolator
+
+######################################################################################################
+#
+# August 1st 2018, RJS
+#
+# This script runs the model optimisation step of SNITCH using the python code emcee written by 
+# Daniel Foreman-Mackey --> http://dfm.io/emcee/current/
+# 
+# You will find that the previously generated look up table values are loaded and then a function 
+# is set up to interpolate over these values. A user can change the interpolation routine how they wish
+# and the lookup function for their needs. 
+# 
+# Similarly, lnprior, lnlikelihood and lnprob functions are defined which the user can change as they need
+# for their science goals. 
+#
+# If you have any questions please email rjsmethurst@gmail.com
+#
+######################################################################################################
+
+
+
+
+padova_zmet = np.array([0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0008, 0.001, 0.0012, 0.0016, 0.0020, 0.0025, 0.0031, 0.0039, 0.0049, 0.0061, 0.0077, 0.0096, 0.012, 0.015, 0.019, 0.024, 0.03])
+padova_zmetsol = 0.019
+padova_solmet = padova_zmet/padova_zmetsol
+
+zmets = (np.linspace(0, 10, 11)*2 + 1).astype(int)
+zsolmets = padova_solmet[(zmets-1).astype(int)] 
+
+time_steps = Planck15.age(10**np.linspace(-0.824, -3.295, 15)).value
+taus = 10**np.linspace(6, 9.778, 50)/1e9
+
+# Load the previous generated look up table spectral parameter values 
+with np.load('spectral_parameter_measurements.npz') as orig_pred:
+    pred = orig_pred['lookup']
+    
+with np.load('spectral_parameter_measurements_mask.npz') as orig_pred:
+    mask = orig_pred['lookupmask']
+
+tqs = np.append(np.flip(time_steps.flatten()[0]- 10**(np.linspace(7, np.log10((time_steps.flatten()[0]-0.1)*1e9), 48))/1e9, axis=0), [time_steps.flatten()[0]-0.001, time_steps.flatten()[0]+0.1], axis=0)
+
+# Create the list of combination of t_obs, tq, tau and Z values taking into account the fact that the tq grid changes everytime t_obs changes
+sv = np.array(list(product([time_steps[0]], tqs, np.log10(taus), zsolmets)))
+for n in range(1, len(time_steps)):
+    tqs = np.append(np.flip(time_steps[n]- 10**(np.linspace(7, np.log10((time_steps[n]-0.1)*1e9), 48))/1e9, axis=0), [time_steps[n]-0.001, time_steps[n]+0.1], axis=0)
+    sv = np.append(sv, np.array(list(product([time_steps[n]], tqs, np.log10(taus), zsolmets))), axis=0)
+    
+
+masked_sp = np.ma.masked_array(data=pred, mask=mask)
+
+# Create the interpolation function which will allow us to calcualte the spectral paramter values at any [t_obs, tq, tau, Z]
+
+f = NearestNDInterpolator(sv, masked_sp)
+
+
+def lookup(theta, age):
+    """
+    Predict the values of the model spectral parameters using the look up table for the test SFH parameters. 
+    
+    INPUTS
+    :theta:
+    An array of size (3,) containing the test values [Z, tq, tau].
+
+    :age:
+    Observed age of a galaxy, often calculated from the redshift i.e. at z=0.1 the age ~ 12.5. Must be in units of Gyr. An array of shape (1,).
+    
+    RETURNS:
+    The predicted spectral parameter values calculated using interpolation over the look up table. 
+    """
+    pred = f([age, theta[1], theta[2], theta[0]])[0]
+
+    ha_pred = pred[0]
+    d4000_pred = pred[6]
+    hb_pred = pred[1]
+    hdA_pred = pred[5]
+    mgfe_pred = np.sqrt( pred[2] * ( 0.72*pred[3] + 0.28*pred[4] )  )  
+    
+    return ha_pred, d4000_pred, hb_pred, hdA_pred, mgfe_pred
+
+    
+def lnlikelihood(theta, ha, e_ha, d4000, e_d4000, hb, e_hb, hdA, e_hdA, mgfe, e_mgfe, age):
+    """ 
+    Function for determining the logarithmic likelihood for a given [Z, tq, tau].
+    
+    :theta:
+    An array of size (3,) containing the test values [Z, tq, tau].
+
+    :ha, d4000, hb, hdA, mgfe:
+    Spectral parameter measurements for the spectra the user is trying to fit a SFH to - note that there are 5 measurements. 
+    They do not necessarily have to match the names but the order has to match whatever lookup returns from the look up table. 
+
+    :e_ha, e_d4000, e_hb, e_hdA, e_mgfe:
+    Same as above but for the measurement error on each spectral parameter. 
+
+    :age:
+    Observed age of a galaxy, often calculated from the redshift i.e. at z=0.1 the age ~ 12.5. Must be in units of Gyr. An array of shape (1,).
+    
+    
+    RETURNS:
+    Array containing the likelihood for each galaxy at the given :theta:
+    """
+
+    ha_pred, d4000_pred, hb_pred, hdA_pred, mgfe_pred = lookup(theta, age)
+    return np.nansum([-0.5*np.log(2*np.pi*e_ha**2)-0.5*((ha-ha_pred)**2/e_ha**2) , -0.5*np.log(2*np.pi*e_d4000**2)-0.5*((d4000-d4000_pred)**2/e_d4000**2), -0.5*np.log(2*np.pi*e_hb**2)-0.5*((hb-hb_pred)**2/e_hb**2), -0.5*np.log(2*np.pi*e_hdA**2)-0.5*((hdA-hdA_pred)**2/e_hdA**2), -0.5*np.log(2*np.pi*e_mgfe**2)-0.5*((mgfe-mgfe_pred)**2/e_mgfe**2)])
+
+# Overall likelihood function combining prior and model
+def lnprobability(theta, ha, e_ha, d4000, e_d4000, hb, e_hb, hdA, e_hdA, mgfe, e_mgfe, age):
+    """
+    Overall logarithmic posterior function combining the prior and calculating the likelihood. 
+        
+        :theta:
+        An array of size (3,) containing the test values [Z, tq, tau].
+
+        :ha, d4000, hb, hdA, mgfe:
+        Spectral parameter measurements for the spectra the user is trying to fit a SFH to - note that there are 5 measurements. 
+        They do not necessarily have to match the names but the order has to match whatever lookup returns from the look up table. 
+
+        :e_ha, e_d4000, e_hb, e_hdA, e_mgfe:
+        Same as above but for the measurement error on each spectral parameter. 
+   
+        :age:
+        Observed age of a galaxy, often calculated from the redshift i.e. at z=0.1 the age ~ 12.5. Must be in units of Gyr. An array of shape (1,).
+        
+        RETURNS:
+        Value of the posterior function for the given :theta: value.
+        
+        """
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + lnlikelihood(theta, ha, e_ha, d4000, e_d4000, hb, e_hb, hdA, e_hdA, mgfe, e_mgfe, age)
+
+def lnprior(theta):
+    """ 
+    Function to calcualte the logarithmic prior on theta values of the Z, tq and tau parameters. Defined ranges are specified 
+    - outside these ranges the function returns -np.inf and does not calculate the posterior probability. 
+       
+    INPUTS 
+        :theta: 
+        An array of size (3,) containing the test values [Z, tq, tau].
+
+    RETURNS:
+        Value of the prior at the specified :theta: value.
+        """
+    Z, tq, tau = theta
+    if 0.003 <= tq <= 13.807108309208775 and np.log10(0.00001) <= tau <= np.log10(5.9) and 0.001 <= Z <= 1.6:
+        return 0.0
+    elif 0.003 <= tq <= 13.807108309208775 and np.log10(5.9) < tau <= np.log10(6.0) and 0.001 <= Z <= 1.6:
+        return 2*(np.exp(np.log10(5.9)) - np.exp(np.log10(tau)))
+    else:
+        return -np.inf
+
+def sample(path, ndim=3, nwalkers=100, nsteps=100, burnin=500, start=[1.0, 13.0, np.log10(1.0)], ha=np.nan, e_ha=np.nan, d4000=np.nan, e_d4000=np.nan, hb=np.nan, e_hb=np.nan, hdA=np.nan, e_hdA=np.nan, mgfe=np.nan, e_mgfe=np.nan, age=Planck15.age(0).value, ID=0):
+    """ 
+    Function to implement the emcee EnsembleSampler function for the sample of galaxies input. Burn in is run and calcualted fir the length specified before the sampler is reset and then run for the length of steps specified. 
+        
+    INPUTS
+        :path:
+        Directory path which output should be saved to 
+
+        :ndim:
+        The number of parameters in the model that emcee must find. In the default expsfh case it always 3 with Z, tq, tau.
+        
+        :nwalkers:
+        The number of walkers that step around the parameter space. Must be an even integer number larger than ndim. 
+        
+        :nsteps:
+        The number of steps to take in the final run of the MCMC sampler. Integer.
+        
+        :burnin:
+        The number of steps to take in the inital burn-in run of the MCMC sampler. Integer. 
+        
+        :start:
+        The positions in the Z, tq and tau parameter space to start. An array of shape (1,3).
+         
+        :ha, d4000, hb, hdA, mgfe:
+        Spectral parameter measurements for the spectra the user is trying to fit a SFH to - note that there are 5 measurements. 
+        They do not necessarily have to match the names but the order has to match whatever lookup returns from the look up table. 
+
+        :e_ha, e_d4000, e_hb, e_hdA, e_mgfe:
+        Same as above but for the measurement error on each spectral parameter. 
+
+        :age:
+        Observed age of a galaxy, often calculated from the redshift i.e. at z=0.1 the age ~ 12.5. Must be in units of Gyr. An array of shape (1,).
+        
+        :id:
+        ID number to specify which galaxy this run is for.
+        
+    RETURNS:
+        :samples:
+        Array of shape (nsteps*nwalkers, ndim) containing the positions of the walkers at all steps for each SFH parameter in ndim.
+        
+        """
+
+    print('emcee running...')
+    p0 = [start + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprobability, args=(ha, e_ha, d4000, e_d4000, hb, e_hb, hdA, e_hdA, mgfe, e_mgfe, age))
+    """ Burn in run here..."""
+    pos, prob, state = sampler.run_mcmc(p0, burnin)
+    lnp = sampler.flatlnprobability
+    np.savez(path+'/lnprob_burnin_'+str(ID)+'.npz', lnp=lnp)
+    samples = sampler.chain[:,:,:].reshape((-1,ndim))
+    samples_save = path+'/samples_burn_in_'+str(ID)+'.npz'
+    np.savez(samples_save, samples=samples)
+    sampler.reset()
+    print('Burn in complete...')
+    """ Main sampler run here..."""
+    sampler.run_mcmc(pos, nsteps)
+    lnpr = sampler.flatlnprobability
+    np.savez(path+'/lnprob_run_'+str(ID)+'.npz', lnp=lnpr)
+    samples = sampler.chain[:,:,:].reshape((-1,ndim))
+    samples_save = path+'/samples_'+str(ID)+'.npz'
+    np.savez(samples_save, samples=samples)
+    print('Main emcee run completed.')
+    sampler.reset()
+
+    return samples
+
+
